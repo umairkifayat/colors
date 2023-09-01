367e90f4a83455120eb6cff469b5a264114f314f
umair kifayat <umairkifayat3@gmail.com>
2023-09-01 14:08:57 -0700

    colors

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function red(color) {
+    document.body.style.backgroundColor = "red";
+}
+function blue(color) {
+    document.body.style.backgroundColor = "blue";
+}
+function green(color) {
+    document.body.style.backgroundColor = "green";
+}
+function orange(color) {
+    document.body.style.backgroundColor = "orange";
+}
+function black(color) {
+    document.body.style.backgroundColor = "black";
+}
